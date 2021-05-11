e5c033c2c84714f2f9a7191591915cd6953556ed
nobrien97 <nicholasobrien97@gmail.com>
2021-05-11 23:11:16 +1000

    Save as image to avoid text resizing

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from io import StringIO
 from subprocess import getoutput
 import sys
 from random import randrange
+from PIL import Image, ImageDraw, ImageFont
+from math import ceil
 
 client = discord.Client()
 
@@ -18,15 +20,23 @@ async def on_message(message):
 
     
     if (message.content == '!fortune'):
-        system('clear')
-        cowTypes = getoutput('cowsay -l')[37:] #  Remove the non-type characters
-        cowTypes = cowTypes.split()
+        # Choose a random cow type from the list of all cowsay characters
+        cowTypes = getoutput('cowsay -l')[37:] #  Remove the non-animal characters
+        cowTypes = cowTypes.split() # split into cowsay animals
         typechoice = cowTypes[randrange(0, len(cowTypes), 1)]
-
-        system('clear')
+        # Use our choice to generate a cowsay
         msg = getoutput('fortune | cowsay -f {}'.format(typechoice))
-        await message.channel.send(msg)
-        system('clear')
+
+        # Image generation: calculate approximate length and width of image (12 font size = 16px)
+        msg_length = (msg.count('\n') * 16 ) + ceil(msg.count('\n') * 1/16 )
+        msg_width = len(max(msg.split('\n')) * 8)  + ceil(len(max(msg.split('\n'))) * 2)
+        msgImg = Image.new('RGB', (msg_width, msg_length), (255, 255, 255))
+        msgDraw = ImageDraw.Draw(msgImg)
+        msgFont = ImageFont.truetype("UbuntuMono-R.ttf", 12)
+        msgDraw.text((16, 0), msg, fill=(0, 0, 0, 255), font=msgFont)
+        # TODO: Don't save to hard drive just to load again
+        msgImg.save('/tmp/fortune.png')
+        await message.channel.send(file=discord.File('/tmp/fortune.png'))
 
 # Yeah nah not letting you lot see my ~ultra secret~ Discord Token(tm)
 tokFile = open(path.expanduser('~/Projects/Discord/DISCORD_TOKEN.tok'))
--- src/test_stdoutread.py
+++ src/test_stdoutread.py
@@ -2,9 +2,32 @@ import os
 import sys
 import io
 from subprocess import getoutput
+from PIL import Image, ImageDraw, ImageFont
+from random import randrange
+from math import ceil
+
 
 
 os.system('clear')
 cowTypes = getoutput('cowsay -l')[37:]
 
-print(cowTypes)
+os.system('clear')
+cowTypes = getoutput('cowsay -l')[37:] #  Remove the non-type characters
+cowTypes = cowTypes.split()
+typechoice = cowTypes[randrange(0, len(cowTypes), 1)]
+os.system('clear')
+msg = getoutput('fortune | cowsay -f {}'.format(typechoice))
+
+msgFont = ImageFont.truetype("UbuntuMono-R.ttf", 12)
+msg_length = (msg.count('\n') * 16 ) + ceil(msg.count('\n') * 1/16 )
+msg_width = ceil(msgFont.getsize(max(msg.split('\n')))[0]*1.5)
+print(msg_length)
+print(msg_width)
+print(msg.split('\n'))
+
+msgImg = Image.new('RGB', (msg_width, msg_length), (255, 255, 255))
+msgDraw = ImageDraw.Draw(msgImg)
+
+msgDraw.text((16, 0), msg, fill=(0, 0, 0, 255), font=msgFont)
+
+msgImg.save('test.jpg')
